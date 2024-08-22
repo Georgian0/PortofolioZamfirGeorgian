0cbe17f09327e3868c4909cac923f8e800003ebe
Georgian0 <geo.zam@yahoo.com>
2024-08-22 21:16:25 +0300

    Update Portofolio
    Fixed Project`s visit button

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -14,7 +14,7 @@ export default function Projects() {
                     <div className='project--1--text'>
                         <h2>Flipboard</h2>
                         <p>Flipboard is a news aggregator and social network aggregation company based in Palo Alto, <br /> California, with offices in New York, Vancouver, and Beijing. Its software, also known as Flipboard,<br /> was first released in July 2010. It aggregates content from social media, news feeds, photo sharing<br /> sites, and other websites, presents it in magazine format, and allows users to "flip" through the articles,<br /> images, and videos being shared.</p>
-                        <button><a  href="https://flipboard-project1.netlify.app/" target='_blank'>Visit page</a></button>
+                        <a  href="https://flipboard-project1.netlify.app/" target='_blank'><button>Visit page</button></a>
                     </div>
 
                     
